92970f9a7be9b3b1b3e9308af3bd6e91b83cf534
SoaMariaka19 <hei.soa.2@gmail.com>
2023-08-22 12:03:14 +0300

    feat: using jpql is more powerfull

--- src/main/java/com/prog4/cnaps_db/repositories/EmployeeEntityRepository.java
+++ src/main/java/com/prog4/cnaps_db/repositories/EmployeeEntityRepository.java
@@ -2,8 +2,13 @@ package com.prog4.cnaps_db.repositories;
 
 import com.prog4.cnaps_db.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface EmployeeEntityRepository extends JpaRepository<EmployeeEntity, Long> {
-    EmployeeEntity findByEnd_to_end_id(String id);
-
+    @Query("SELECT e FROM EmployeeEntity e WHERE e.endToEndId = :id")
+    Optional<EmployeeEntity> findEmployeeEntityByEndToEndId(Long id);
 }
