852af1925f9ff564615ca67d33bc4fffa2f917be
YlanNaly <hei.nalisoa@gmail.com>
2023-08-22 11:59:00 +0300

    refactor: moov method to repository impl

--- src/main/java/com/prog4/employee_db/controller/mapper/EmployeeMapper.java
+++ src/main/java/com/prog4/employee_db/controller/mapper/EmployeeMapper.java
@@ -2,9 +2,7 @@ package com.prog4.employee_db.controller.mapper;
 
 import com.prog4.employee_db.controller.model.ModelEmployee;
 import com.prog4.employee_db.entity.*;
-import com.prog4.employee_db.service.CnapsService;
-import com.prog4.employee_db.service.NationalCardService;
-import com.prog4.employee_db.service.SocioProService;
+import com.prog4.employee_db.service.*;
 import com.prog4.employee_db.repository.PhoneNumberRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -19,13 +17,11 @@ import java.util.List;
 public class EmployeeMapper {
 
     private NationalCardService nationalCardService;
-    private CnapsService cnapsService;
     private final PhoneNumberRepository phoneNumberRepository;
     private SocioProService socioProService;
     public Employee toEntity(ModelEmployee employee) throws IOException {
         SocioPro socioPro = socioProService.getByCategory(employee.getSocioProCat());
         socioPro.setCategories(employee.getSocioProCat());
-
         List<PhoneNumber> phoneNumbers = new ArrayList<>();
         for (String number : employee.getPhoneNbr()) {
             PhoneNumber phoneNumber = new PhoneNumber();
@@ -33,8 +29,6 @@ public class EmployeeMapper {
             phoneNumbers.add(phoneNumber);
         }
 
-        Cnaps cnaps = new Cnaps();
-        cnaps.setNbrCNAPS(employee.getNbrCnaps());
         NationalCard nationalCard = new NationalCard();
         nationalCard.setNumber(employee.getCinNumber());
         nationalCard.setDate(employee.getCinDate());
@@ -42,16 +36,12 @@ public class EmployeeMapper {
 
         SocioPro socio = new SocioPro();
         NationalCard nationalCard1 = new NationalCard();
-        Cnaps cnaps1 = new Cnaps();
 
         List<PhoneNumber> savedPhoneNumbers = phoneNumberRepository.saveAll(phoneNumbers);
         if(!savedPhoneNumbers.isEmpty()){
             socio = socioProService.saveSocioPro(socioPro);
             if(socio != null){
                 nationalCard1 = nationalCardService.save(nationalCard);
-                if(nationalCard1 != null){
-                    cnaps1 = cnapsService.save(cnaps);
-                }
             }
         }
 
@@ -78,49 +68,8 @@ public class EmployeeMapper {
                 .outDate(employee.getOutDate())
                 .sex(employee.getSex())
                 .post(employee.getPost())
-                .nbrCnaps(cnaps1)
                 .nbrChildren(employee.getNbrChildren())
                 .address(employee.getAddress())
                 .build();
-    }
-
-    public ModelEmployee convertToEmployeeForm(Employee employee) {
-        List<String> phoneNumbers = new ArrayList<>();
-        for(PhoneNumber number : employee.getPhoneNumbers()){
-            phoneNumbers.add(number.getNumber());
-        }
-        ModelEmployee modelEmployee = new ModelEmployee();
-        modelEmployee.setId(employee.getId());
-        modelEmployee.setLastName(employee.getLastName());
-        modelEmployee.setFirstName(employee.getFirstName());
-        modelEmployee.setSocioPro(employee.getCateSocioPro().getId());
-        modelEmployee.setDateOfBirth(employee.getDateOfBirth());
-        modelEmployee.setSex(employee.getSex());
-        modelEmployee.setRegistrationNbr(employee.getRegistrationNbr());
-        modelEmployee.setPhoneNbr(phoneNumbers);
-        modelEmployee.setAddress(employee.getAddress());
-        modelEmployee.setPays(employee.getPays());
-        modelEmployee.setEmailPerso(employee.getEmailPerso());
-        modelEmployee.setEmailPro(employee.getEmailPro());
-        modelEmployee.setBeggingDate(employee.getBeggingDate());
-        modelEmployee.setOutDate(employee.getOutDate());
-        modelEmployee.setNbrChildren(employee.getNbrChildren());
-
-        if (employee.getCateSocioPro() != null) {
-            modelEmployee.setSocioProCat(socioProService.getById(employee.getCateSocioPro().getId()).getCategories());
-        }
-        if (employee.getCin() != null) {
-            modelEmployee.setCinNumber(employee.getCin().getNumber());
-            modelEmployee.setCinDate(employee.getCin().getDate());
-            modelEmployee.setCinPlace(employee.getCin().getPlace());
-        }
-        if (employee.getPost() != null) {
-            modelEmployee.setPost(employee.getPost());
-        }
-        if (employee.getNbrCnaps() != null) {
-            modelEmployee.setNbrCnaps(employee.getNbrCnaps().getNbrCNAPS());
-        }
-
-        return modelEmployee;
     }
 }
